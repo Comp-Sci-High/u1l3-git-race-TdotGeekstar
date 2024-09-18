794c6b5f715f0c4e10e61bbfaa150bef5c7f0cc8
TdotGeekstar <tom.saoura26@compscihigh.org>
2024-09-18 15:19:37 +0000

    Favs

--- index.js
+++ index.js
@@ -4,10 +4,10 @@
 // Stage and commit this change with an appropriate message
 // Task 2
 // Print your favorite snack
-
+Fruits
 // Stage and commit this change with an appropriate message
 // Task 3
 // Print your favorite artist
-
+Kendrick Lamar
 // Stage and commit this change with an appropriate message
 // Go to more.js
